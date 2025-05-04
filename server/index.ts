8b7a16332b3dc4dd96b37b1456d19f5243abaced
ni30kosh <nitish@getkosh.com>
2025-05-04 07:26:47 +0530

    cord handle

--- server/index.ts
+++ server/index.ts
@@ -13,6 +13,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
   origin: [
     'http://localhost:5173',
+    'https://calender-check.vercel.app/',
     'https://task-calendar-app.vercel.app',
     'https://calender-check-nhoushdnd-ni30guptas-projects.vercel.app'
   ],
